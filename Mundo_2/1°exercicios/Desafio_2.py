6ac4974229a7e353da0ddb05f79de34a456952ff
Artrineias <erik21yan@gmail.com>
2021-06-22 14:48:19 -0300

    1,2

--- Mundo_2/1°exercicios/Desafio_2.py
+++ Mundo_2/1°exercicios/Desafio_2.py
@@ -1,15 +1,15 @@
 
-numero = int(input('Digite um numero: '))
-print('''Escolha o padrão numerico: 
-[1] conversor para binario.
-[2] conversor para octal.
-[3] conversor para hexadecimal''')
-opção = int(input('Sua escolha: '))
-if opção == 1:
-    print('O numero {} é {} em binario'.format(numero, bin(numero)[2:]))
-elif opção == 2:
-    print('O numero {} é {} em octal'.format(numero, oct(numero)[2:]))
-elif opção == 3:
-    print('O numero {} é {} em hexadecimal'.format(numero, hex(numero)[2:]))
+number = int(input('Enter a number: '))
+print('''choose the numerical pattern: 
+[1] convert to binary.
+[2] convert to octal.
+[3] convert to hexadecimal''')
+option = int(input('your choice: '))
+if option == 1:
+    print('The number {} is {} in binary'.format(number, bin(number)[2:]))
+elif option == 2:
+    print('The number {} is {} in octal'.format(number, oct(number)[2:]))
+elif option == 3:
+    print('The number {} is {} in hexadecimal'.format(number, hex(number)[2:]))
 else:
-    print('Opção invalida.')
+    print('Invalid option.')
