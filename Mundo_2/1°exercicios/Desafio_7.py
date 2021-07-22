08f12b64e9fd36b5dd8978e5afe690938c66750c
Artrineias <46578175+Artrineias@users.noreply.github.com>
2021-07-22 10:24:17 -0300

    Delete Desafio_7.py

--- Mundo_2/1°exercicios/Desafio_7.py
+++
@@ -1,9 +0,0 @@
-print('De o valor das medidas dos lados.')
-t_1 = float(input('1°lado:'))
-t_2 = float(input('2°lado:'))
-t_3 = float(input('3°lado:'))
-
-if t_1 < t_2 + t_3 and t_3 < t_2 + t_1 and t_2 < t_1 + t_3:
-    print('Esse triangulo   existe!!!')
-else:
-    print('Esse triangulo não existe!!!')
