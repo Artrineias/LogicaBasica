8f2d9617797e5440284bd26a1089fd58290ff3df
Artrineias <erik21yan@gmail.com>
2021-09-16 22:23:59 -0300

    16/10

--- Mundo_2/while/Validação_de_dado.py
+++ Mundo_2/while/Validação_de_dado.py
@@ -4,12 +4,12 @@ c = 1
 
 while (c == 1):
     os.system('clear')or None
-    sexo = input('Difite o seu sexo: ').upper()
+    sexo = input('Digite o seu sexo(M = Masculino e F = Feminino): ').upper()
     if sexo == 'M' or sexo == 'F':
         c = c - 1
         os.system('clear')or None
         print('Dado salvo!!!')
-        sleep(3)
+        sleep(1)
         os.system('clear')or None 
     else:
         os.system('clear')or None
