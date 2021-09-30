672483f3743fffbb566e662df70446e2faf25411
Artrineias <erik21yan@gmail.com>
2021-09-30 09:23:03 -0300

    3009

--- Mundo_3/Tuplas/Analise_dados_de_tupla.py
+++ Mundo_3/Tuplas/Analise_dados_de_tupla.py
@@ -5,11 +5,20 @@ numbers = (number[0],number[1],number[2],number[3])
 nine = even = 0
 for count in range(0,4):
     if numbers[count]==3:
-        three = count
+        three = 0
+        if three != count:
+            three = count
+            print(f'The value 3 appeared in the {three + 1}th position.')
+    else:
+        print('The value 3 does not appear at all.')
+
     if numbers[count] == 9:
-        nine += 1 
+        nine += 1
+    
     if numbers[count]%2 == 0:
-        even += 1
-print(f'The value 9 appeared {nine} times.')
-print(f'The value 3 appeared in the {three + 1}th position.')
-print(f'The value of even numbers entered was {even}.')
+        print(f'The value of even numbers entered was {count}.')
+
+if nine!=0:     
+    print(f'The value 9 appeared {nine} times.')
+else:
+    print(f'The value 9 does not appear once.') 
