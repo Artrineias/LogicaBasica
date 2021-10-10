330365bfeac8203e7ce722b4b68d035e5e7fe0ce
Artrineias <erik21yan@gmail.com>
2021-10-10 08:46:10 -0300

    RUIM10100845

---
+++ Mundo_3/Lista/valores unicos em listas.py
@@ -0,0 +1,17 @@
+from typing import Counter
+
+
+lista = []
+count = 0
+while True:
+    save = count
+    while count == 0:
+        if 
+            lista.pop()
+            lista.append(int(input("This value is equal to another value in the list, enter another: ")))
+    lista.append(int(input("Enter a value: ")))
+    decision = input("Want to Continue(Y/N): ").upper()
+    count = save
+    count += 1
+    if decision == "N":
+        break
