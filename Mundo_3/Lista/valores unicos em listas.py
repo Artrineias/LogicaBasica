b9a4f6a925b18f472b6a9a6aeb209480fa740036
Artrineias <erik21yan@gmail.com>
2021-10-11 16:16:52 -0300

    11101616

--- Mundo_3/Lista/valores unicos em listas.py
+++ Mundo_3/Lista/valores unicos em listas.py
@@ -2,16 +2,23 @@ from typing import Counter
 
 
 lista = []
-count = 0
-while True:
-    save = count
-    while count == 0:
-        if 
-            lista.pop()
-            lista.append(int(input("This value is equal to another value in the list, enter another: ")))
-    lista.append(int(input("Enter a value: ")))
+decision = 0
+while True:    
+    
+    while True:
+        if decision == 0:
+            value =int(input("Enter a value: "))
+        
+        if value in lista:
+            value = int(input("To this value in the list, type another value: "))
+            decision = 1
+        elif value not in lista:
+            lista.append(value)
+            decision = 0
+            break
+
     decision = input("Want to Continue(Y/N): ").upper()
-    count = save
-    count += 1
     if decision == "N":
         break
+    elif decision == "Y" :
+        a = []
