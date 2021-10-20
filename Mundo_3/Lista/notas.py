825b60ef7c90873faf98067c3b623a4da78da8b6
Artrineias <erik21yan@gmail.com>
2021-10-20 10:02:39 -0300

    20101002

--- Mundo_3/Lista/notas.py
+++ Mundo_3/Lista/notas.py
@@ -1,34 +1,26 @@
-name = []
-nota = []
-c = b = 0
+ficha = []
 while True:
     
-    name.append(input("Enter a name: "))
-    nota.append(float(input("1°note: ")))
-    nota.append(float(input("2°note: ")))
-    nota.append((nota[0]+nota[1])/2)
-    name.append(nota[:])
-    nota.clear()
+    name = input("Enter a name: ")
+    nota_1 = float(input("1°note: "))
+    nota_2 = float(input("2°note: "))
+    media =  (nota_2 + nota_1)/2
+    ficha.append([name,[nota_1,nota_2],media])
     
     decision = input("Want to continion: ").upper()
     if decision == "N":
         break
 
 print("-="*25)
-print("N.o   Name       Average" )
+print(f'{"N.o":<4}   {"Name":<10}{"Average":>8}' )
 print("-"*25)
-while b < (len(name)/2):
-    print(f"{b: <6}{name[c]: <11}{name[c+1][2]}")
-    b += 1
-    c +=2
+for i,j in enumerate(ficha):
+    print(f"{i:<4}{j[0]:<10}{j[2]:>8.1f}")
 print("-"*25)
-a = 0
-while a != 999:
-    a = int(input("Show a student's grade: "))
-    if a <= b :
-        if a == 0:
-            print(f"The grades of {name[a*2]} is {name[(a*2)+1][0]},{name[(a*2)+1][1]}")
-        elif a%2 == 0:
-            print(f"The grades of {name[a*2]} is {name[(a*2)+1][0]},{name[(a*2)+1][1]}")
-        else:
-            print(f"The grades of {name[a*2]} is {name[(a*2)+1][0]},{name[(a*2)+1][1]}")
+while True:
+    opc = int(input("Mostrar notas de qual aluno?(999 interrompe):"))
+    if opc== 999:
+        print("End...")
+        break
+    if opc <= len(ficha) - 1:
+        print(f"Notas do {ficha[opc][0]} são {ficha[opc][1]}.")
