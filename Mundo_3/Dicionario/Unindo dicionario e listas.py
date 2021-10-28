3c170b47230a00f059b22e4bfcbc5356bb93bd4e
Artrineias <erik21yan@gmail.com>
2021-10-28 20:02:04 -0300

    28102002

--- Mundo_3/Dicionario/Unindo dicionario e listas.py
+++ Mundo_3/Dicionario/Unindo dicionario e listas.py
@@ -6,15 +6,16 @@ while True:
     fix["sex"] = input("Sex:").upper()
     while fix["sex"] not in "MF":
         del fix["sex"]
-        print("Error, please just type Y or N.")
+        print("Error, please just type M or F.")
         fix["sex"] = input("Sex:").upper()
     fix["age"] = int(input("Age:"))
     dados.append(fix.copy())
     ida += fix["age"] 
-    decicao = input("Want to continue:").upper()
-    del fix["name"]
-    del fix["age"]
-    del fix["sex"]
+    while True:    
+        decicao = input("Want to continue:").upper()
+        if decicao in "NY":
+            break
+        print("Error, please just type N or Y")
     if decicao == "N":
         break    
 media = ida/(len(dados))
@@ -22,19 +23,19 @@ print("-="*30)
 print(f"A) Everyone has {len(dados)} registered people")
 print(f"B) The average age is {media}")
 print("C) The women registered were: ",end="")
-for i in range(0,len(dados)):
-    if dados[i]["sex"] == "F":
-        print(f"{dados[i]['name']}",end="")
+for i in dados:
+    if i["sex"] == "F":
+        print(f"{i['name']}",end="")
         f += 1 
 if f == 0:
     print("none")
 else:
     print("")
 print("D) List of people who are above average age:")
-for i in range(0,len(dados)):
-    if dados[i]["age"] > media:
+for i in dados:
+    if i["age"] > media:
         print("->   ",end="")
-        for k,v in dados[i].items():
+        for k,v in i.items():
             print(f"{k} : {v};",end=" ")
         print("")
 
