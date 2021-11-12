58298f07f783f11b4d891725ee97bd1dd247a3be
Artrineias <erik21yan@gmail.com>
2021-11-12 16:52:13 -0300

    12111652

---
+++ Mundo_3/Função/cadastro de jogadores.py
@@ -0,0 +1,14 @@
+
+def reader(p,g):
+    if p == "":
+        p = "<Unknown>"
+    
+    if g == "" or (g.isnumeric()) == False:
+        g = "0"
+    print(f"Player {p} scored {g} goal(s) in the championship.")
+
+
+#main
+p = input("player: ")
+g = input("Goals: ")
+reader(p,g)
