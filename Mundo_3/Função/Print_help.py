e5a435e7ed7037b36e0c336bcb11645b970e7aa8
Artrineias <erik21yan@gmail.com>
2021-11-16 19:23:35 -0300

    16111923

---
+++ Mundo_3/Função/Print_help.py
@@ -0,0 +1,27 @@
+c = [   '\033[m' #Padrão
+        '\033[0;0m'# restara
+        '\033[1;40m', #Preto  
+        '\033[1;41m', #Vermelho 
+        '\033[1;42m', #Verde 
+        '\033[1;43m', #Amarelo 
+        '\033[1;44m', #Azul 
+        '\033[1;45m', #Magenta 
+        '\033[1;46m', #Cyan 	 	    
+ 		'\033[1;47m', #Cinza Claro
+
+]
+
+
+def ajuda (com):
+    help(com)
+
+
+def titulo (text,cor=0):
+    tam =len(text) + 4
+    print('~'*tam)  
+    print(f"  {text}  ")
+    print('~'*tam)
+
+#main
+
+titulo("ola mundo")
