aef107c22342db77eb7e84c40e40ee922f69e2f2
Artrineias <erik21yan@gmail.com>
2021-11-16 20:52:45 -0300

    16112052

--- Mundo_3/Função/Print_help.py
+++ Mundo_3/Função/Print_help.py
@@ -1,3 +1,4 @@
+from time import sleep
 c = [   '\033[m' #Padrão
         '\033[0;0m'# restara
         '\033[1;40m', #Preto  
@@ -19,9 +20,15 @@ def ajuda (com):
 def titulo (text,cor=0):
     tam =len(text) + 4
     print('~'*tam)  
-    print(f"  {text}  ")
+    print(f"  {text}")
     print('~'*tam)
-
+    sleep(1)
 #main
-
-titulo("Função do Python --> ")
+while True: 
+    titulo("sistema de ajuda PyHELP")
+    funcao=input("Função do Python --> ")
+    sleep(2)
+    if funcao.upper() == "FIM":
+        break
+    titulo(f"Acessando o manual do comando {funcao}")
+    ajuda(funcao)
