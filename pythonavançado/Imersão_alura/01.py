13ce3b6b99b52ab8766cc6144e12646cbc03feaa
Artrineias <erik21yan@gmail.com>
2022-06-05 12:01:19 -0300

    biblioteca nova

--- pythonavançado/Imersão_alura/01.py
+++ pythonavançado/Imersão_alura/01.py
@@ -1,4 +1,9 @@
-from selenium.webdriver import Firefox
-from webdriver_manager.firefox import GeckoDriverManager
+import pandas as pd
 
-browser = Firefox(executable_path=GeckoDriverManager().install()) 
+
+url = "https://gist.githubusercontent.com/tgcsantos/3bdb29eba6ce391e90df2b72205ba891/raw/22fa920e80c9fa209a9fccc8b52d74cc95d1599b/dados_imoveis.csv"
+dados = pd.read_csv(url)
+busca_vila_mariana = (dados["Bairro"]== "Vila Mariana")
+vila_mariana = dados[busca_vila_mariana]
+numeros_de_imoveis_por_bairro = dados["Bairro"].value_counts()
+print(numeros_de_imoveis_por_bairro.plot.hist())
