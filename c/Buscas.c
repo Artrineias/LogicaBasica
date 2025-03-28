574015bd628dcf738a274829a838fe2d8e670d3e
Artrineias <erik21yan@gmail.com>
2025-03-28 12:37:36 -0300

    ordenador  criado

--- c/Buscas.c
+++ c/Buscas.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
-int lista[10] = [1,2,3,3,2,1,5,6,7,4];
+int lista[10] = {1,2,3,3,2,1,5,6,7,4};
 int posicoes[10];
 
 int ordenacao(){
     int aux = 0;
-    
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            if(lista[i]<lista[j]){
+                aux = lista[j];
+                lista[j] = lista[i];
+                lista[i] = aux;
+            }
+        }
+    }
+    for(int i = 0;i<10;i++){
+        printf("%d",lista[i]);
+    }
 }
 
 int buscabinaria(){
@@ -18,7 +29,7 @@ int sequencial(){
 
 int main(){
 
-    
+    ordenacao();
 
 
     return 0;
--- c/OrdenacaoVetor.c
+++ c/OrdenacaoVetor.c
@@ -2,12 +2,12 @@
 
 
 int main(){
-    int vetor[5] = {1,5,4,7,3};
+    int vetor[5] = {5,2,3,6};
     int anterior ;
     
-    for (int i = 0; i < 5; i++){
-        for(int j = 0; j <5;j++){
-            if(vetor[i]>vetor[j]){
+    for (int i = 0; i < 4; i++){
+        for(int j = 0; j <4;j++){
+            if(vetor[i]<vetor[j]){
                 anterior = vetor[j];
                 vetor[j] = vetor[i];
                 vetor[i] = anterior;
