ee92827335b94c144dd40d703d37529ae2f447c2
Artrineias <erik21yan@gmail.com>
2025-04-29 12:22:00 -0300

    final vetordim

--- c/vetordimensao.c
+++ c/vetordimensao.c
@@ -1,46 +1,48 @@
 #include "stdio.h"
 #include <stdlib.h>
 
-double * criar(int n){
-    double *v;
-    v = malloc(n*sizeof(double));
+int * criar(int n){
+    int *v;
+    v = malloc(n*sizeof(int));
     return v;
 }
 
-double * vetor( double v[],int n){
-    printf("Digite %d Valores: ",n);
-    for (int l; l<n; l++) {
-        fgets(v, n,stdin);
+int * vetor( int v[],int n){
+    for (int l = 0; l<n; l++) {
+        printf("Digite Valores: ",n);
+        scanf("%d",&v[l]);
     }
     return v;
 }
 
-double * soma(double *v1,double *v2,int n){
-    double *soma; 
+int * soma(int *v1,int *v2,int n){
+    int sun = 0,*s; 
     for (int i= 0; i<n ; i++) {
-        
+        sun = v1[i] + v2[i]+ sun;
     }
-    
-    return soma;
+    printf("a soma é essa %d\n",sun);
+    s = &sun;
+    return s;
 }
 
-double * aloca(int n){
-    double *v1,*v2,*saida;
+int * aloca(int n){
+    int *v1,*v2,*saida;
     v1 = vetor(criar(n), n);
     v2 = vetor(criar(n), n);
-    saida = soma(v1,v2);
+    saida = soma(v1,v2,n);
 
+    return saida;
 
 }
 
 int main(){
 
     int n;
-    double *v1,*v2;
+    int v;
 
     scanf("%d",&n);
 
-    aloca(n);
+    v = *aloca(n);
 
 
 
